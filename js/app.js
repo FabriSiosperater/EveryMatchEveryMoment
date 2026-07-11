8ad5ec4097a70dcb9a06e21dd4b65a71c523cb92
FabriSiosperater <cfabriziorr@gmail.com>
2026-07-11 13:12:36 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
+/*
+=========================================
+Every Match Every Moment
+Versión: v0.2 Kickoff
+Archivo: app.js
+=========================================
+*/
 
+function showScreen(screenId) {
+
+    // Obtener todas las pantallas
+    const screens = document.querySelectorAll(".screen");
+
+    // Ocultarlas
+    screens.forEach(screen => {
+        screen.classList.remove("active");
+    });
+
+    // Mostrar la pantalla seleccionada
+    const selectedScreen = document.getElementById(screenId);
+
+    if (selectedScreen) {
+        selectedScreen.classList.add("active");
+    }
+
+}
